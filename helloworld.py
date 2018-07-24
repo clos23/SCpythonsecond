8abf7567f368d9ef869fcfc23daedafee49fca1e
Carlos Mendoza <mendozac@cbchs.org>
2018-07-24 14:31:54 +0000

    first commit

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+#First Computer Program
+
+print("Hello World")
+print("Your mission is if you choose to accept it")
+
+print 2+4
+print 2+(4*7)
+print 2**2
+print 10%3
+
+x = 3
+y = 3
+if x == y:
+     print ("Yay")
+else:
+     print ("Nay")
+     
+name = ("Ms Kimbery")
+print name
+ 
+x = 2**2
+y= 2+2
+if x== y:
+    print("Nice!")
+    name = ("Good job, Bob")
+    print name
+else:
+    print("Nope,")  
+    print("Try Again Bob") 
